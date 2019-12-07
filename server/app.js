685f90b400dd4a6db4b22636af5fb567d4254d5c
Saju Paul <saju.christel@blu-pa.com>
2019-12-07 20:33:53 +0530

    saving images statically

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path =require('path');
+
 const feedRoutes = require('./routes/feed');
 
 const app = express();
@@ -19,6 +21,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 mongoose
   .connect(
