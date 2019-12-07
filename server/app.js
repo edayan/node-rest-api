bf040efeba71a20cda05635245c12e383c8187d3
Saju Paul <saju.christel@blu-pa.com>
2019-12-07 19:32:01 +0530

    create mongoose, connect to db

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 const feedRoutes = require('./routes/feed');
 
 const app = express();
@@ -9,12 +9,25 @@ const app = express();
 app.use(bodyParser.json()); // application/json
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect(
+    ''
+  )
+  .then(resulr => {
+    console.log('db connection successfull');
+    app.listen(8080, () => {
+      console.log(`server started on the port 8080`);
+    });
+  })
+  .catch(err => console.log('db connection failed', err));
